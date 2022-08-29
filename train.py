4b609553ae7fe007ff277bcb02003647444a0c35
John Wu <johnwu0604@gmail.com>
2022-08-29 08:36:46 -0700

    Added confusion matrix

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
-
 import argparse
-import os
-import sys
+import json
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,10 +8,8 @@ import torch.optim as optim
 import mlflow
 import wandb
 from torchvision import datasets, transforms
-
-url = "https://activeeon-public.s3.eu-west-2.amazonaws.com/datasets/MNIST.new.tar.gz"
-os.system("wget -O MNIST.tar.gz {}".format(url))
-os.system("tar -zxvf MNIST.tar.gz")
+from azureml.core import Run
+from sklearn.metrics import confusion_matrix
 
 class Net(nn.Module):
     def __init__(self):
@@ -33,6 +29,22 @@ class Net(nn.Module):
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
+def load_data(args, kwargs):
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])), 
+            batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, 
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])),
+            batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    return train_loader, test_loader
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -48,6 +60,13 @@ def train(args, model, device, train_loader, optimizer, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 batch_idx / len(train_loader), loss.item()))
 
+    # Log training metrics        
+    mlflow.log_metrics({
+        "Epoch": epoch,
+        "Train Loss": loss.item()})
+    wandb.log({
+        "Epoch": epoch,
+        "Train Loss": loss.item()})
 
 def test(args, model, device, test_loader):
     model.eval()
@@ -58,9 +77,7 @@ def test(args, model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            # sum up batch loss
             test_loss += F.nll_loss(output, target, reduction='sum').item()
-            # get the index of the max log-probability
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -69,6 +86,7 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         correct / len(test_loader.dataset)))
 
+    # Log testing metrics
     mlflow.log_metrics({
         "Test Accuracy": 100. * correct / len(test_loader.dataset),
         "Test Loss": test_loss})
@@ -76,9 +94,37 @@ def test(args, model, device, test_loader):
         "Test Accuracy": 100. * correct / len(test_loader.dataset),
         "Test Loss": test_loss})
 
+def evaluate():
+    
+    # Facing issues getting model outputs into confusion matrix. Mocking some evaluation metrics for now.
+
+    # Log confusion matrix
+    y_true = [2, 0, 2, 2, 0, 1, 3, 4, 5, 6, 8, 7, 9, 4, 5, 2, 3, 1, 7, 8, 9, 8, 6, 4]
+    y_pred = [0, 0, 2, 2, 0, 2, 3, 3, 6, 8, 8, 7, 9, 6, 5, 2, 8, 1, 7, 3, 9, 8, 6, 4]
+    labels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    # Using AML APIs
+    cm = {
+        "schema_type": "confusion_matrix",
+        "schema_version": "1.0.0",
+        "data": {
+            "class_labels": labels,
+            "matrix": confusion_matrix(y_true, y_pred).tolist()
+        }
+    }
+    run = Run.get_context()
+    run.log_confusion_matrix('Confusion Matrix', json.dumps(cm))
+
+    # Using Wandb
+    cm = wandb.plot.confusion_matrix(
+        y_true=y_true,
+        preds=y_pred,
+        class_names=labels)
+    wandb.log({"conf_mat": cm})
 
 def main():
 
+    # Set experiment/project
     mlflow.set_experiment('pytorch-mnist')
     wandb.init(project='pytorch-mnist')
 
@@ -103,36 +149,22 @@ def main():
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
+    # Log parameters
     mlflow.log_params(vars(args))
     wandb.config.update(args)
 
     torch.manual_seed(args.seed)
-
     device = torch.device("cuda" if use_cuda else "cpu")
-
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])),
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    train_loader, test_loader = load_data(args, kwargs)
 
     model = Net().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.lr,
-                          momentum=args.momentum)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
         test(args, model, device, test_loader)
-
+    evaluate()
 
 if __name__ == '__main__':
     main()
